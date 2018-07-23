bdecb3424e61f03a60653847d07dde048575155a
Kiana Kaslana <kiana@ninetaillabs.com>
2018-07-23 08:05:47 +0200

    More code
    - Added another method

--- LinkedLibrary/ExampleClass.cs
+++ LinkedLibrary/ExampleClass.cs
@@ -16,5 +16,18 @@ namespace LinkedLibrary
         {
             await Task.Delay(durationToWait);
         }
+
+        /// <summary>
+        /// Does some calculation and return some value
+        /// </summary>
+        /// <param name="value1">First value</param>
+        /// <param name="value2">Second value</param>
+        /// <returns>Some answer</returns>
+        public int DoSomeMaths(int value1, int value2)
+        {
+            var result1 = value1 + value2;
+            var result2 = value1 * value2;
+            return result1 + result2;
+        }
     }
 }
