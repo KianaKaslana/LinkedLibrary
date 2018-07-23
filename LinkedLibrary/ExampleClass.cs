69c32a93b8da3aed5a75242da724fb7dd59e9d09
Kiana Kaslana <kiana@ninetaillabs.com>
2018-07-23 07:49:08 +0200

    Code commit
    - Code checkin

---
+++ LinkedLibrary/ExampleClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LinkedLibrary
+{
+    /// <summary>
+    /// Basic example class
+    /// </summary>
+    public class ExampleClass
+    {
+        /// <summary>
+        /// Wait for a specified amount of time
+        /// </summary>
+        /// <param name="durationToWait">The time to wait for</param>
+        public async Task WaitForMeAsync(TimeSpan durationToWait)
+        {
+            await Task.Delay(durationToWait);
+        }
+    }
+}
